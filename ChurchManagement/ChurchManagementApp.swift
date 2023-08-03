256db5015d210dc8a67ba0cc1ff79a682d68cf8c
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-03 14:10:32 -0600

    Initial Commit

---
+++ ChurchManagement/ChurchManagementApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChurchManagementApp.swift
+//  ChurchManagement
+//
+//  Created by Guillermo Ruiz Baires on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ChurchManagementApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
