2d0836d1036aeb4c89053a5e5ac7d182ce6e37ca
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-05 12:50:46 -0600

    MemberView

---
+++ ChurchManagement/Member/MemberView.swift
@@ -0,0 +1,20 @@
+//
+//  MemberView.swift
+//  ChurchManagement
+//
+//  Created by Guillermo Ruiz Baires on 5/8/23.
+//
+
+import SwiftUI
+
+struct MemberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemberView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemberView()
+    }
+}
