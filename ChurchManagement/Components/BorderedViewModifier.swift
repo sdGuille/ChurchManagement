33c17fb86e0c038ab69a952fd8544c3556d3a8b4
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-04 21:57:17 -0600

    Modifier for buttons

---
+++ ChurchManagement/Components/BorderedViewModifier.swift
@@ -0,0 +1,16 @@
+//
+//  Bordered.swift
+//  ChurchManagement
+//
+//  Created by Guillermo Ruiz Baires on 4/8/23.
+//
+
+
+import SwiftUI
+
+struct BorderedViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        
+    }
+}
