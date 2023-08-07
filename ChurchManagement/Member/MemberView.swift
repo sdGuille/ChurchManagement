f21c7f0a4bd86447d8b102961cca0711ef725645
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-07 12:07:43 -0600

    Basic UI member view

--- ChurchManagement/Member/MemberView.swift
+++ ChurchManagement/Member/MemberView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct MemberView: View {
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                HStack {
+                    Text("GR")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 72, height: 72)
+                        .background(Color(.systemGray3))
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Guillermo Ruiz")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .padding(.top, 4)
+                        
+                        Text("Diacono")
+                            .font(.footnote)
+                            .tint(.gray)
+                        
+                    }
+                }
+                
+            }
+            
+            Section("Informacion Personal") {
+                Text("Edad: 30")
+                Text("Estado civil: Casado")
+                Text("Telefono: 69654804")
+            }
+            
+            
+            Section("Informacion Ministerial") {
+                Text("Es Bautizado: Si")
+                Text("Bautizado en el Espiritu Santo: Si")
+                Text("Esta actualmente en Discipulado: No")
+            }
+            
+        }
+        .formStyle(.automatic)
     }
 }
 
