a164a045766161a5ba120deca33180c45ba26d28
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-12 00:35:01 -0600

    Create Form

---
+++ ChurchManagement/Create/View/CreateView.swift
@@ -0,0 +1,66 @@
+//
+//  CreateView.swift
+//  ChurchManagement
+//
+//  Created by Guillermo Ruiz Baires on 11/8/23.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    @State private var age = Date(timeIntervalSince1970: 0)
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                firstname
+                lastname
+                job
+                phone
+                
+                Section {
+                    DatePicker("Fecha de Nacimiento", selection: $age, displayedComponents: .date)
+                        .datePickerStyle(.compact)
+                    
+                }
+                .listSectionSeparator(.visible)
+                
+                Section {
+                    submit
+                }
+            }
+            .navigationTitle("Crear")
+        }
+    }
+}
+
+struct CreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateView()
+    }
+}
+
+private extension CreateView {
+    
+    var firstname: some View {
+        TextField("First name", text: .constant(""))
+    }
+    
+    var lastname: some View {
+        TextField("Last name", text: .constant(""))
+    }
+    
+    var phone: some View {
+        TextField("Phone number", text: .constant(""))
+    }
+    
+    var job: some View {
+        TextField("Job", text: .constant(""))
+    }
+    
+    var submit: some View {
+        Button("Submit") {
+            // TODO: Handle action
+        }
+    }
+}
