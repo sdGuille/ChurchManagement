7ed666c32032fb2fda9378f8bc0f75078ddb04bc
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-15 15:30:54 -0600

    Update ChurchManagementApp.swift

--- ChurchManagement/App/ChurchManagementApp.swift
+++ ChurchManagement/App/ChurchManagementApp.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 @main
 struct ChurchManagementApp: App {
-//    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             HomeView()
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environment(\.managedObjectContext, MembersProvider.shared.viewContext)
         }
     }
 }
