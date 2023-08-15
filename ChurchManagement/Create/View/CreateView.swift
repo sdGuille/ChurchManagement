94239d210f5cc31093e13e21ddb8014c4b92ea01
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-15 14:48:13 -0600

    Member provider with NSPersistent

--- ChurchManagement/Create/View/CreateView.swift
+++ ChurchManagement/Create/View/CreateView.swift
@@ -18,9 +18,7 @@ struct CreateView: View {
                 job
                 phone
                 birthDate
-                
-                
-                
+
                 Section {
                     submit
                 }
---
+++ ChurchManagement/Provider/MembersProvider.swift
@@ -0,0 +1,29 @@
+//
+//  MemberProvider.swift
+//  ChurchManagement
+//
+//  Created by Guillermo Ruiz Baires on 15/8/23.
+//
+
+import Foundation
+import CoreData
+
+final class MembersProvider {
+    static let shared = MembersProvider()
+    
+    private let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "ChurchManagement")
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+        persistentContainer.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Unable to load store with error: \(error)")
+            }
+        }
+    }
+}
