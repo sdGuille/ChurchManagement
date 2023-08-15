e7d4d3b730d86992a2aa05ec5d99a3b18ec5831b
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-15 14:16:13 -0600

    Member from Entity

---
+++ ChurchManagement/Create/Model/Member.swift
@@ -0,0 +1,29 @@
+//
+//  Member.swift
+//  ChurchManagement
+//
+//  Created by Guillermo Ruiz Baires on 15/8/23.
+//
+
+import Foundation
+import CoreData
+
+final class Member: NSManagedObject {
+    @NSManaged var birthdate: Date
+    @NSManaged var esBautizado: Bool
+    @NSManaged var esCasado: Bool
+    @NSManaged var id: UUID
+    @NSManaged var isMember: Bool
+    @NSManaged var ministerio: String
+    @NSManaged var name: String
+    @NSManaged var phoneNumber: String
+    
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        setPrimitiveValue(Date.now, forKey: "birthdate")
+        setPrimitiveValue(false, forKey: "esBautizado")
+        setPrimitiveValue(false, forKey: "esCasado")
+        setPrimitiveValue(false, forKey: "isMember")
+    }
+}
