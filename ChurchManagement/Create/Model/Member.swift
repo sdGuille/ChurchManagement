3ff725231e33452659fbec3c4137bc7ce3ceba84
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-08-17 18:09:17 -0600

    Update Member.swift

--- ChurchManagement/Create/Model/Member.swift
+++ ChurchManagement/Create/Model/Member.swift
@@ -9,21 +9,22 @@ import Foundation
 import CoreData
 
 final class Member: NSManagedObject {
+    
+    @NSManaged var job: String
     @NSManaged var bautizadoES: Bool
     @NSManaged var birthdate: Date
-    @NSManaged var esBautizado: Bool
     @NSManaged var esCasado: Bool
     @NSManaged var id: UUID
     @NSManaged var isMember: Bool
     @NSManaged var ministerio: String
     @NSManaged var name: String
+    @NSManaged var lastname: String
     @NSManaged var phoneNumber: String
     
     override func awakeFromInsert() {
         super.awakeFromInsert()
         
         setPrimitiveValue(Date.now, forKey: "birthdate")
-        setPrimitiveValue(false, forKey: "esBautizado")
         setPrimitiveValue(false, forKey: "esCasado")
         setPrimitiveValue(false, forKey: "isMember")
         setPrimitiveValue(false, forKey: "bautizadoES")
